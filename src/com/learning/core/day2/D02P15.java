bde1b57c905cf2b777744a0258c6f7dff5b6b6b3
Aniket Roy <aniketarko@gmail.com>
2024-03-04 18:14:25 +0530

    File added

--- src/com/learning/core/day2/D02P15.java
+++ src/com/learning/core/day2/D02P15.java
@@ -2,32 +2,30 @@ package com.learning.core.day2;
 import java.util.Scanner;
 public class D02P15 {
 
-	public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        int positiveCount = 0;
-        int negativeCount = 0;
-        int zeroCount = 0;
-        
-        char choice;
-        do {
-            System.out.print("Enter a number: ");
-            int number = scanner.nextInt();
-            
-            if (number > 0) {
-                positiveCount++;
-            } else if (number < 0) {
-                negativeCount++;
-            } else {
-                zeroCount++;
-            }
-            
-            System.out.print("Do you want to enter another number? (Y/N): ");
-            choice = scanner.next().charAt(0);
-        } while (choice == 'Y' || choice == 'y');
-        
-        System.out.println("Number of positive numbers: " + positiveCount);
-        System.out.println("Number of negative numbers: " + negativeCount);
-        System.out.println("Number of zero numbers: " + zeroCount);
-    }
-}
+	 public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        
+	        System.out.print("Enter the number of elements: ");
+	        int n = scanner.nextInt();
+	        
+	        int positiveCount = 0;
+	        int negativeCount = 0;
+	        int zeroCount = 0;
+	        
+	        System.out.println("Enter the elements:");
+	        for (int i = 0; i < n; i++) {
+	            int num = scanner.nextInt();
+	            if (num > 0) {
+	                positiveCount++;
+	            } else if (num < 0) {
+	                negativeCount++;
+	            } else {
+	                zeroCount++;
+	            }
+	        }
+	        
+	        System.out.println("Number of positive numbers: " + positiveCount);
+	        System.out.println("Number of negative numbers: " + negativeCount);
+	        System.out.println("Number of zero numbers: " + zeroCount);
+	    }
+	}
