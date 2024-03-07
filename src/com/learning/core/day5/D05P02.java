1c6cf3c7700e84c6d98a3405b303fd4a2deb474d
Aniket Roy <aniketarko@gmail.com>
2024-03-07 22:13:00 +0530

    File added

---
+++ src/com/learning/core/day5/D05P02.java
@@ -0,0 +1,23 @@
+package com.learning.core.day5;
+import java.util.ArrayList; 
+import java.util.List; 
+import java.util.Scanner; 
+public class D05P02 {
+	public static void main(String[]args) 
+	 { 
+	  ArrayList<String> al=new ArrayList(); 
+	  System.out.println("Enter the names "); 
+	  Scanner sc=new Scanner(System.in); 
+	  String[] input=sc.nextLine().split(" "); 
+	  for(String name:input) 
+	  { 
+	   al.add(name); 
+	  } 
+	  System.out.println(al); 
+	  System.out.println("Enter the name to search"); 
+	  String search=sc.nextLine(); 
+	   
+	   
+	 } 
+	 
+	}
